5f16623bb0f071041679d58a12825f88ff9125ee
a-poor <45295232+a-poor@users.noreply.github.com>
2020-12-11 12:18:32 -0500

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="toydb",
-    version="0.0.2",
+    version="0.0.3",
     author="Austin Poor",
     author_email="austinpoor@gmail.com",
     description="A small, toy database written in pure python.",
--- toydb/__init__.py
+++ toydb/__init__.py
@@ -5,4 +5,4 @@ from . import dtypes
 from . import exceptions
 from .Database import Database
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
