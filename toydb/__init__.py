cc0d631d5e5e6321169077d82fb2c742624cd272
a-poor <45295232+a-poor@users.noreply.github.com>
2020-12-22 22:46:00 -0500

    Adding package functions

--- toydb/__init__.py
+++ toydb/__init__.py
@@ -1,10 +1,20 @@
 """
 """
 
+import pathlib as _pathlib
+import shutil as _shutil
+
 from . import dtypes
-from . import util
 from . import exceptions
 from .Database import Database
 from .RowStruct import RowStruct
 
 __version__ = "0.1.0"
+
+def create_db(dbname="database.tdb",path="."):
+    """Create a new database"""
+    return Database(dbname,path)
+
+def delete_db(db: Database):
+    """Delete a database"""
+    db.remove()
